70eec2e7b98704fbb00421d385d1227eb8e38f62
Aman <apraka01@student.ubc.ca>
2024-02-04 11:12:04 -0800

    Updated endpoints.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -54,7 +54,7 @@ function App() {
 
   useEffect(() => {
     const getUser = async () => {
-      fetch("https://marketplace.a-ramcreatives.com/auth/login/success", {
+      fetch("http://marketplace.a-ramcreatives.com/auth/login/success", {
         method: "GET",
         credentials: "include",
         headers: {
